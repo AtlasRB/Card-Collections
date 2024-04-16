72ef5c0671289f28ce19e82dded2ce4b0495461e
Connor Yeeles <connoryeeles@outlook.com>
2024-04-16 16:09:19 +0100

    Adjusted scaling for smaller screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -39,16 +39,16 @@ function App() {
           <button onClick={() => handleFilterChange('true')} className="p-3 w-48 text-white bg-blue-700 hover:bg-blue-800 rounded-lg">Show Collected</button>
           <button onClick={() => handleFilterChange('false')} className="p-3 w-48 text-white bg-blue-700 hover:bg-blue-800 rounded-lg">Show Not Collected</button>
         </div>
-        <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder="Type to search by name" className="p-1 w-96 rounded-md"/>
-        <input type="text" value={searchNumber} onChange={(e) => setSearchNumber(e.target.value)} placeholder="Type to search by number" className="p-1 w-96 rounded-md"/>
+        <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder="Type to search by name" className="p-1 w-72 md:w-96 rounded-md"/>
+        <input type="text" value={searchNumber} onChange={(e) => setSearchNumber(e.target.value)} placeholder="Type to search by number" className="p-1 w-72 md:w-96 rounded-md"/>
         <button type="button" onClick={handleInputChange} className="p-1 w-48 bg-white hover:bg-slate-100 rounded-md">Search</button>
           <table className="bg-slate-100 border border-black border-2 mb-10">
               <thead>
                   <tr className="border-b-2">
                       <th className="border border-black border-y-2">Name</th>
-                      <th className="border border-black border-y-2 px-2 md:px-10">Number</th>
+                      <th className="border border-black border-y-2 px-1 md:px-10">Number</th>
                       <th className="border border-black border-y-2">Type</th>
-                      <th className="border border-black border-y-2 px-2 md:px-10">Collected</th>
+                      <th className="border border-black border-y-2 px-1 md:px-10">Collected</th>
                   </tr>
               </thead>
               <tbody>
@@ -58,9 +58,9 @@ function App() {
                           (filterOption === 'false' && !card.collected)) {
                           return (
                               <tr key={card.id}>
-                                  <td className="border border-black px-2 md:px-10">{card.name}</td>
+                                  <td className="border border-black px-1 md:px-10">{card.name}</td>
                                   <td className="border border-black text-center">{card.number}</td>
-                                  <td className="border border-black px-2 md:px-10">{card.type}</td>
+                                  <td className="border border-black px-1 md:px-10">{card.type}</td>
                                   <CollectedColour collected={card.collected}/>
                               </tr>
                           );
